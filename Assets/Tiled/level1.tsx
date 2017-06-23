f54062e0d80d89b79a4f0e95e9709fcad5e49a3d
Arael Sidh <threetimesinfinite@gmail.com>
2017-06-22 22:02:16 -0300

    Tilemap mockup
    * Tiled2Unity implementation

---
+++ Assets/Tiled/level1.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="16" tileheight="16" tilecount="2030" columns="58">
+ <image source="superpowers-asset-packs-master/prehistoric-platformer/background-elements/tileset-1mod.png" width="928" height="560"/>
+ <tile id="1044">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="16"/>
+   <object id="8" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1045">
+  <objectgroup draworder="index">
+   <object id="4" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1046">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1047">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1164">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1165">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1222">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1223">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
