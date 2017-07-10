ecbee70815379c4f979096a54a1957498b258023
Arael Sidh <threetimesinfinite@gmail.com>
2017-07-10 18:41:46 -0300

    Trex AI (fixed)
     * Instantiator logic refactored just a bit

--- Assets/Scripts/EntityGenerator.cs
+++ Assets/Scripts/EntityGenerator.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class EntityGenerator : MonoBehaviour {
 
 	private GameObject currentInstance;
+	private bool canDisableInstance = true;
+	public bool CanDisableInstance { get { return canDisableInstance; } }
+
 	public GameObject generatorPrefab;
 	public Transform entityGroupRef;
 
 	void Awake () {
 		currentInstance = Instantiate (generatorPrefab, transform.position, transform.rotation, entityGroupRef);
+		switch (generatorPrefab.name) {
+		case "Trex":
+			TrexEnemyScript tes = currentInstance.GetComponent<TrexEnemyScript> ();
+			tes.eg = this;
+			break;
+		}
 		currentInstance.SetActive (false);
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.tag == "AliveArea")
+			canDisableInstance = true;
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.tag == "AliveArea") {
-			if (currentInstance == null)
+			if (currentInstance == null) {
 				currentInstance = Instantiate (generatorPrefab, transform.position, transform.rotation, entityGroupRef);
+				switch (generatorPrefab.name) {
+				case "Trex":
+					TrexEnemyScript tes = currentInstance.GetComponent<TrexEnemyScript> ();
+					tes.eg = this;
+					break;
+				}
+			}
 			else
 				currentInstance.SetActive (true);
+			canDisableInstance = false;
 		}
 	}
 
-	void OnTriggerExit2D(Collider2D col) {
-		if (col.tag == "AliveArea") {
-			if (currentInstance != null) {
-				currentInstance.SetActive (false);
-			}
-		}
-	}
+
 }
--- Assets/Scripts/TrexEnemyScript.cs
+++ Assets/Scripts/TrexEnemyScript.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class TrexEnemyScript : BaseEnemyScript {
 
 	private bool roamLeft;
+	private bool canDisableInstance = true;
+	public bool CanDisableInstance { get { return canDisableInstance; } }
 	public GameObject leftAttackTrigger;
 	public GameObject rightAttackTrigger;
+	public EntityGenerator eg;
 
 	void Start () {
 		hp = 3;
@@ -45,17 +48,42 @@ public class TrexEnemyScript : BaseEnemyScript {
 			Roam ();
 			break;
 		}
+
+		if (this.canDisableInstance && eg.CanDisableInstance)
+			gameObject.SetActive (false);
 	}
 
 	void ChangeDirection() {
-		
+		sr.flipX = !sr.flipX;
+		roamLeft = sr.flipX;
+		rightAttackTrigger.SetActive (!roamLeft);
+		leftAttackTrigger.SetActive (roamLeft);
 	}
 
+	//	POR QUÉ MIERDAS NECESITO PONERLE VELOCIDAD CADA FRAME?
 	void Roam() {
-
+		rb.velocity = new Vector2 (Globals.trexSpeedX * (roamLeft ? -1 : 1) * Time.deltaTime, 0);
+		animator.SetFloat ("SpeedAbsX", Mathf.Abs (rb.velocity.x));
 	}
 
 	void Attack () {
 		status = Status.Attacking;
+	}
+
+	void OnTriggerStay2D(Collider2D col) {
+		if (col.tag == "AliveArea")
+			this.canDisableInstance = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.tag == "StopSignal")
+			ChangeDirection ();
+		else if (col.tag == "AliveArea")
+			this.canDisableInstance = false;
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		if (col.tag == "AliveArea")
+			this.canDisableInstance = true;
 	}
 }
